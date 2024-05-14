97c936a5a32937777f0d3a450b6092aeaa26b70b
futuroprogramadorwz <142597436+futuroprogramadorwz@users.noreply.github.com>
2024-05-14 09:57:27 -0300

    Add files via upload

---
+++ 1-array.js
@@ -0,0 +1,6 @@
+let produtos =['laranja', "maça", 'banana']
+
+for (let i=0; i <produtos.length; i++)
+{
+    console.log(produtos[i])
+}
---
+++ 2-array-push-unshift.js
@@ -0,0 +1,15 @@
+
+var frutas = [];
+
+
+frutas.push('Maçã');
+frutas.push('Banana');
+frutas.push('Laranja');
+
+console.log('Array após adicionar elementos ao final;', frutas);
+
+
+frutas.unshift('Pêssego');
+frutas.unshift('Abacaxi');
+
+console.log('Array após adicionar elementos ao inicio:', frutas)
