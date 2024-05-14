03a09a4c8b462499ad7ced154373259771850dc9
futuroprogramadorwz <142597436+futuroprogramadorwz@users.noreply.github.com>
2024-05-14 09:57:58 -0300

    Delete 1-funcoes_1.js

--- 1-funcoes_1.js
+++
@@ -1,9 +0,0 @@
-//declaração da função
-function imprimeTexto(texto) {
-  console.log(texto);
-}
-
-//chamada da função - execução da função uma ou mais vezes
-
-imprimeTexto("Olá!!")
-imprimeTexto("Outro texto!!!")
